2dc3d804ab1f3d8aafcdf65c8ae38fa921fb86b1
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-03-12 22:17:41 -0500

    Cascaron apiclient

---
+++ src/main/resources/static/js/apiclient.js
@@ -0,0 +1,9 @@
+const url = 'http://localhost:8080/blueprints/';
+
+var apiclient =(function(){
+    return{
+        getBlueprintsByAuthor : function(authname,callback){
+            
+        }
+    }
+})();
